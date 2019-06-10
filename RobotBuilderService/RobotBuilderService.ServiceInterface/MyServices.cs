98637a3e2ad1c5f9ce4b87f18d8c8abc2c205fa8
Mayank Gupta <mayank.gupta04@nagarro.com>
2019-06-10 11:52:02 +0530

    Add project files.

---
+++ RobotBuilderService/RobotBuilderService.ServiceInterface/MyServices.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack;
+using RobotBuilderService.ServiceModel;
+
+namespace RobotBuilderService.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
