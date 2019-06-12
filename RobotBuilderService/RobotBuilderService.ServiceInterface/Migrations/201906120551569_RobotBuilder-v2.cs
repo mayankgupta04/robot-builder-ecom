93275cf8f5c34d70f1846486d10a8e1db29e6caf
gauravmakhijaNagarro <gaurav.makhija@nagarro.com>
2019-06-12 11:22:56 +0530

    updated RobotBuilderv1 DbMigration and created v2 migration.

---
+++ RobotBuilderService/RobotBuilderService.ServiceInterface/Migrations/201906120551569_RobotBuilder-v2.cs
@@ -0,0 +1,16 @@
+namespace RobotBuilderService.ServiceInterface.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RobotBuilderv2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
